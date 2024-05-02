e1c65da94d459c2d8475b3c42eab5d64f46321cf
Matias Sabbadini <matiassabbadini24@gmail.com>
2024-05-02 15:52:39 -0300

    cambios en authController

--- src/app/utils/header.ts
+++ src/app/utils/header.ts
@@ -3,8 +3,8 @@ import { HttpHeaders } from '@angular/common/http';
 export function getHeaders(): HttpHeaders {
   const user = localStorage.getItem('currentUser')
   const currentUser = JSON.parse(user!);
-  const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', currentUser.token);
-  console.log("este es el user" + currentUser)
-  console.log("este es el header" + headers)
+  const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', currentUser!.token);
+  console.log("este es el user " + JSON.stringify(currentUser));
+  console.log("este es el header " + JSON.stringify(headers));
   return headers;
 }
