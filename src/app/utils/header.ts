06e24fe9863b1b7262f78899b60420e447224298
Matias Sabbadini <matiassabbadini24@gmail.com>
2024-05-02 15:21:21 -0300

    cambios en authController

--- src/app/utils/header.ts
+++ src/app/utils/header.ts
@@ -7,6 +7,7 @@ export function getHeaders(): HttpHeaders {
   if (user && user.token) {
     headers.set('Authorization', user.token);
   }
-
+  console.log(user)
+  console.log(headers)
   return headers;
 }
